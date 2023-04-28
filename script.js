3fbf6e6e6879b4e08076e4108e27137fea82a213
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-04-28 16:04:21 +0200

    Creation array

---
+++ script.js
@@ -0,0 +1,38 @@
+const Team = [
+	{
+		Name: 'Wayne Barnett',
+		Posizion: 'Founder & CEO',
+		photo: wayne-barnett-founder-ceo.jpg,
+	},
+
+    {
+		Name: 'Angela Caroll',
+		Posizion: 'Chief Editor',
+		photo: angela-caroll-chief-editor.jpg,
+	},
+
+    {
+		Name: 'Walter Gordon',
+		Posizion: 'Office Manager',
+		photo: walter-gordon-office-manager.jpg,
+	},
+
+    {
+		Name: 'Angela Lopez',
+		Posizion: 'Social Media Manager',
+		photo: angela-lopez-social-media-manager.jpg,
+	},
+
+    {
+		Name: 'Scott Estrada',
+		Posizion: 'Developer',
+		photo: scott-estrada-developer.jpg,
+	},
+
+    {
+		Name: 'Barbara Ramos',
+		Posizion: 'Graphic Designer',
+		photo: barbara-ramos-graphic-designer.jpg,
+	},
+	
+];
