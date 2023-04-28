7a1b689026262b1006e086170f672357245b295d
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-04-28 16:14:03 +0200

    console.log print

--- script.js
+++ script.js
@@ -2,37 +2,48 @@ const Team = [
 	{
 		Name: 'Wayne Barnett',
 		Posizion: 'Founder & CEO',
-		photo: wayne-barnett-founder-ceo.jpg,
+		Photo: 'wayne-barnett-founder-ceo.jpg',
 	},
 
     {
 		Name: 'Angela Caroll',
 		Posizion: 'Chief Editor',
-		photo: angela-caroll-chief-editor.jpg,
+		Photo: 'angela-caroll-chief-editor.jpg',
 	},
 
     {
 		Name: 'Walter Gordon',
 		Posizion: 'Office Manager',
-		photo: walter-gordon-office-manager.jpg,
+		Photo: 'walter-gordon-office-manager.jpg',
 	},
 
     {
 		Name: 'Angela Lopez',
 		Posizion: 'Social Media Manager',
-		photo: angela-lopez-social-media-manager.jpg,
+		Photo: 'angela-lopez-social-media-manager.jpg',
 	},
 
     {
 		Name: 'Scott Estrada',
 		Posizion: 'Developer',
-		photo: scott-estrada-developer.jpg,
+		Photo: 'scott-estrada-developer.jpg',
 	},
 
     {
 		Name: 'Barbara Ramos',
 		Posizion: 'Graphic Designer',
-		photo: barbara-ramos-graphic-designer.jpg,
+		Photo: 'barbara-ramos-graphic-designer.jpg',
 	},
 	
 ];
+
+printTeamMember(Team);
+
+function printTeamMember (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let employee = arr[i];
+        console.log(employee.Name);
+        console.log(employee.Posizion);
+        console.log(employee.Photo);
+    }
+}
